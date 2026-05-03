6f2a72b7fc2004c2ef04cfa12cbcec75e027d752
swayamtejwani <swayam.tejwani@gmail.com>
2026-05-03 14:44:44 +0530

    Update homepage project count

--- template-home.php
+++ template-home.php
@@ -43,7 +43,7 @@ get_header();
 </div>
 <div class="text-right">
 <p class="text-white text-label-md uppercase opacity-80">Projects</p>
-<p class="text-white font-black text-2xl">100+</p>
+<p class="text-white font-black text-2xl">90+</p>
 </div>
 </div>
 </div>
